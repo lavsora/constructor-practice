3919c4c7f4bcb7f4443e2aa72f3995e0a50e37b2
lavsora <ya.karpich@gmail.com>
2022-06-18 19:11:42 +0300

    refactor infinity scroll

--- script.js
+++ script.js
@@ -40,32 +40,28 @@ const DomElement = function (selector, height, width, bg, fontSize, position) {
         document.body.style.overflow = 'hidden';
 
         if (event.keyCode === 38) {
-            newElement.style.top = upDown - 10 + 'px';
+            newElement.style.top = `${upDown - 10}px`;
         }
         if (event.keyCode === 40) {
-            newElement.style.top = upDown + 10 + 'px';
+            newElement.style.top = `${upDown + 10}px`;
         }
         if (event.keyCode === 39) {
-            newElement.style.left = rightLeft + 10 + 'px';
+            newElement.style.left = `${rightLeft + 10}px`;
         }
         if (event.keyCode === 37) {
-            newElement.style.left = rightLeft - 10 + 'px';
+            newElement.style.left = `${rightLeft - 10}px`;
+        }
+        
+        if (upDown <= -newElement.style.height.substr(-6, 3) - 1) {
+            newElement.style.top = `${innerHeight - 1}px`
+        } else if (upDown >= innerHeight){
+            newElement.style.top = `-${newElement.style.height}`;
         }
 
-        if (upDown <= -90) {
-            upDown = 660
-            newElement.style.top = upDown + 'px'
-        } else if (upDown >= 750){
-            upDown = 0
-            newElement.style.top = upDown + 'px'
-        }
-
-        if (rightLeft <= -90) {
-            rightLeft = 1440
-            newElement.style.left = rightLeft + 'px'
-        } else if (rightLeft >= 1530){
-            rightLeft = 0
-            newElement.style.left = rightLeft + 'px'
+        if (rightLeft <= -newElement.style.width.substr(-6, 3) - 1) {
+            newElement.style.left = `${innerWidth - 1}px`
+        } else if (rightLeft >= innerWidth){
+            newElement.style.left = `-${newElement.style.width}`
         }
     }
 }
